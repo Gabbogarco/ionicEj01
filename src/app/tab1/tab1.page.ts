d441f4d2ef6be4e288593324a77d9f1727b44753
Gabbo <liggc@hotmail.com>
2019-03-08 14:41:29 -0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
-
+import{AnimalesService} from '../api/animales.service'
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
-})
-export class Tab1Page {}
+  	selector: 'app-tab1',
+  	templateUrl: 'tab1.page.html',
+  	styleUrls: ['tab1.page.scss']
+	})
+export class Tab1Page {
+		Oanimales:any;
+
+	constructor(private servicio_animales:AnimalesService){
+
+	}
+	ngOnInit(){
+    	  	this.servicio_animales.getAnimales().subscribe(data=>{
+        	console.log(data);
+      		this.Oanimales=data.animales;
+        	console.log(this.Oanimales)
+      });
+   }
+}
